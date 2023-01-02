e3903c061abded20d9df7ec786e90f20fbe7344b
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2023-01-02 21:08:50 +0100

    add schema tabel manager

---
+++ Models/SchemaManager/__init__.py
@@ -0,0 +1,19 @@
+
+from Models.SqlManager import SqlContext
+
+# Schema is a user Model present an sql table :
+
+
+class Schema:
+    def __init__(self, db_uri="",schema_config={}, sql_manager=SqlContext()):
+        # schema config is an python dict as 
+        """
+        {
+            tables:[]
+            Models:[]
+            State:[]
+        }
+        """
+        self.db_uri = db_uri
+        self.configuration = schema_config
+        self.sqlHandler
