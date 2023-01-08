147545a5221935968253bbcee36055427bfb8c9b
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2023-01-08 21:29:26 +0100

    Update command-syntax manager

--- Models/SyntaxManager/__init__.py
+++ Models/SyntaxManager/__init__.py
@@ -1,6 +1,42 @@
-
-
+from rich.console import Console
+from Models.Providers.Provider import Provider
 class CommandManager:
-    def manage_command(self,command=""):
-        command_arr = command.split(' ')
-        return
+    # define the alowed commands : 
+    def __init__(self):
+        """
+        define all command and flags allowed : 
+        
+        """
+        self.Allowed_command = {
+            'make':['Modal','Migration'],
+            'create':['Table','DataBase']
+        }
+        self.Printer = Console()
+    def manage_command(self, command_arr=[],global_config={}):
+        # check command :
+        if command_arr[0] == 'manager':
+            # check the rest :
+            # command must be a base-command:flag
+            flag_sliter = command_arr[1].split(':')
+            # check if flag_spliter is a base command : 
+            if flag_sliter[0] in self.Allowed_command:
+                # check if the flag is allowed by the command base :
+                if flag_sliter[1] in self.Allowed_command[flag_sliter[0]]:
+                    # manage the create providers :
+                    if flag_sliter[0] == 'create' and flag_sliter[1] == 'DataBase':
+                        # call provider static methos to manage task ;
+                        Provider.CreateDatabase(global_config['database_uri'])
+                        self.Printer.print(
+                            f"\t[bold blue] { flag_sliter[0] } => { flag_sliter[1] } => { global_config['database_uri']  } [/] \n")
+                    return
+                else:
+                    raise Exception(
+                        f' { flag_sliter[1] } not supported by { flag_sliter[0]} ')
+            else:
+                raise Exception(
+                    f'{ flag_sliter[0] } is not a valid command or option :( '
+                )
+        else:
+            raise Exception(
+                'all Pylite3 command must started with manager'
+            )
