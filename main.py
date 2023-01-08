d245fd2283e7df997ba9e91156bdbb5ebff0be11
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2023-01-08 21:29:49 +0100

    update entry script :)

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import json
 import errno
 
-
+from Models.ManagerModel.manage import Manager
 # define my input manager class : 
 
 class inputHandller:
@@ -14,12 +14,17 @@ class inputHandller:
         self.project_path = project_path
         self.entry_script = entry_script
         self.my_console = Console()
-        self.save_state()
+    @staticmethod
+    def get_config():
+        # this method is allowd only if the project already initialized 
+        # read json data from config/data_file.json:
+        with open('config/data_file.json', 'r') as source_data:
+            return json.load(source_data)
     def save_state(self):
         # save folder path and main script as an json file attributes :
         self.my_console.print(
             f"\n\t [bold black] {self.project_path} and {self.entry_script} has been initialized  [/]\n")
-        data = {
+        self.data = {
             'path':self.project_path,
             'main':self.entry_script,
             'database_uri':'Database/my_db.sqlite',
@@ -35,8 +40,7 @@ class inputHandller:
             if exc.errno == errno.EEXIST and os.path.isdir(directory_config):
                 pass
         with open("config/data_file.json", "w") as write_file:
-            json.dump(data, write_file)
-        return
+            json.dump(self.data, write_file)
 # execution state : 
 
 if __name__ == "__main__":
@@ -75,6 +79,7 @@ if __name__ == "__main__":
                     f" \n\t[underline black]{ file_input }[/] [bold blue] selected as main script   [/] \n")
                 # all is good : we ned to create our manager :
                 my_handler = inputHandller(folder_path,file_input)
+                my_handler.save_state()
         if len(sys.argv) > 2:
             # is an configuration command :
             # step 1 : check if the folder has ben initialised :
@@ -84,8 +89,13 @@ if __name__ == "__main__":
                     configuration_state = True
                     break
             if configuration_state == False:
-                my_console.print("\n\t [black] your folder has not inisialized yet :(  [/]")
+                my_console.print("\n\t [black] your folder has not inisialized yet :(  [/] \n")
+                exit()
             else:
-                my_console.print("\n\t [bold blue] managing your command :) [/]")
+                # start managing command by the manager class :
+                config_data = inputHandller.get_config()
+                my_manager = Manager(config_data,sys.argv)
+                # start :
+                my_manager.start_watch()
     except  Exception as e:
-        my_console.print("\n\t[bold red][error] ==> "+str(e)+"[/]")
+        my_console.print("\n\t[bold red][error] ==> "+str(e)+"[/] \n")
