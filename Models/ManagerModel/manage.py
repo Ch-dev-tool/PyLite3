ea0a7cf7e591202b5fa4bc1226f7e5e7a3e01ea9
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2023-01-08 21:27:59 +0100

    Update manager

--- Models/ManagerModel/manage.py
+++ Models/ManagerModel/manage.py
@@ -2,10 +2,11 @@
 from Models.SyntaxManager import CommandManager
 from Models.SchemaManager import Schema
 from Models.SqlManager import SqlContext
+from rich.console import Console
 # create my class manager : 
 
 class Manager:
-    def __init__(self, config={}):
+    def __init__(self, config={},arg_list=[]):
         # expected config dict : 
         """
         {
@@ -18,6 +19,8 @@ class Manager:
         }
         """
         self.current_configuration = config
+        self.my_console = Console()
+        self.system_args = arg_list
         self.syntax_handler = CommandManager()
         self.schema_handler = Schema(
                                     db_uri=self.current_configuration['database_uri'],
@@ -29,7 +32,14 @@ class Manager:
                                     sql_manager=SqlContext(db_uri=self.current_configuration['database_uri']))
         # for now all configuration done :) 
     # define methode to start all services :
-    def start_watch(self, user_command=""):
+    def start_watch(self):
         # start my syntaxe manager:
-        self.syntax_handler.manage_command(user_command)
-        return         
+        # the command is an splited string => is an lsit :
+        # filter my argument : ignore python main.py execution comand:
+        self.system_args = self.system_args[1:]
+        try:
+            self.syntax_handler.manage_command(
+                self.system_args, self.current_configuration)
+        except  Exception as e:
+            self.my_console.print("\n\t[bold red][error] ==> "+str(e)+"[/] \n")
+        return 
