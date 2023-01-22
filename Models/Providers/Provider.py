c6174a2f509e1f84182cf6c8c68fd51b0c58e52e
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2023-01-23 00:01:29 +0100

    update Provider ( create db method )

--- Models/Providers/Provider.py
+++ Models/Providers/Provider.py
@@ -1,9 +1,21 @@
-
+import os
+import errno
 
 class Provider:
     @staticmethod
     def CreateDatabase(db_uri=""):
-        print(
-            "\n  run run .......... \n"
-        )
-        return
+        created_state = False
+        if len(db_uri)==0:
+            raise Exception(
+                'Database uri must be a valid path'
+            )
+        # create the folder container :
+        try:
+            os.mkdir(db_uri.split('/')[0])
+        except OSError as exc:
+            if exc.errno == errno.EEXIST:
+                pass
+        # create new file as a data base : 
+        with open(db_uri, "a+") as created_data_base:
+            created_state = True
+        return created_state
