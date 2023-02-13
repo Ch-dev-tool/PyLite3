967b90f314d5bb1b27b9c0e6229d365bc6b0d700
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2023-02-13 20:58:42 +0100

    update Provider static methods

--- Models/Providers/Provider.py
+++ Models/Providers/Provider.py
@@ -21,10 +21,32 @@ class Provider:
         return created_state
     @staticmethod
     def CheckModel(modal_list=[],table_name=""):
-        for modal_item in modal_list:
-            if modal_item == table_name:
-                return True
-        return  False
+        return  table_name in modal_list
+    @staticmethod
+    def AppendModal(global_config={},modal_name=""):
+        # create new migration
+        new_modals_list = global_config['modals']
+        new_migration_list.append(modal_name)
+        print(f"\n  { modal_name } { new_modals_list }")
+        migration = dict(
+            name = f"Create New Table for modal : { modal_name }",
+            state=False
+        )
+        new_migration_list = global_config['migrations']
+        print(f"\n { migration } { new_migration_list }")
+        # update global config file : 
+        current_data = {
+            'path': global_config['path'],
+            'main': global_config['main'],
+            'database_uri': global_config['database_uri'],
+            'tables': [],
+            'modals': new_modals_list,
+            'migrations': new_migration_list
+        }
+        print(f"\n { current_data }")
+        with open("../../config/data_file.json", "w+") as write_file:
+            json.dump(current_data, write_file)
+        return f" config/data_file.json has been Updated -> New Modal created "
     @staticmethod
     def MakeModal(project_uri="",modal_name=""):
         check_state = 404
@@ -41,7 +63,7 @@ class Provider:
             try:
                 os.mkdir(directory_modal)
                 check_state -= 100
-                # step 4 reate __init__.py to define modal ad a python pack 
+                # step 4 create __init__.py to define modal ad a python pack 
                 with open(directory_modal+"/__init__.py","w") as write_file:
                     check_state += 99
             except OSError as exc:
